0ac1acb62bf8707b09cbf39455d149f2b6cc28e7
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-08-14 17:40:11 -0500

    build: setup tw and basic module structure

---
+++ src/modules/core/components/header.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export interface HeaderProps{
+    className?: string;
+}
+
+export function Header({
+    children,
+    className,
+}: PropsWithChildren<HeaderProps>) {
+    return (
+        <h1 className={twMerge("text-3xl text-primary font-bold", className)}>{children}</h1>
+    )
+}
