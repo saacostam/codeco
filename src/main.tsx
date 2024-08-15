f657ffa4186e2b83e9ad3131a1f230d382a1575d
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-08-14 21:17:09 -0500

    feat(algo): create a list of algorithms and show them in home page

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ const router = createHashRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Navbar/>
-    <div className="max-w-7xl bg-base-200 rounded-2xl p-4 m-8 mx-auto">
+    <div className="max-w-7xl bg-base-200 rounded-2xl p-8 m-8 mx-auto">
       <RouterProvider router={router}/>
     </div>
   </StrictMode>,
--- src/modules/pages/home.tsx
+++ src/modules/pages/home.tsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
+import { ALGORITHMS } from "../algorithms";
 import { Header } from "../core";
 
 export function HomePage(){
     return (
-        <Header className="text-center">Home</Header>
+        <>
+            <Header className="text-center mb-8">Available Algorithms</Header>
+            {
+                ALGORITHMS.map(({ id, title, description }) => (
+                    <div className="card bg-base-100 shadow-xl" key={id}>
+                        <div className="card-body">
+                            <h2 className="card-title">{title}</h2>
+                            <p>{description}</p>
+                            <div className="card-actions justify-end">
+                                <Link to={id} className="btn btn-primary">Demo</Link>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </>
     )
 }
